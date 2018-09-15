a4141d30a9c08a60182c059bd59f235f4419a610
Nicole Gutierrez <nicolegutierrez1999@gmail.com>
2018-09-15 03:20:06 -0600

    version1

---
+++ Proyecto2/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Proyecto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.b_dibujar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.text_y2 = new System.Windows.Forms.TextBox();
+            this.text_x2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.text_x1 = new System.Windows.Forms.TextBox();
+            this.text_y1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.lblOrigen);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.b_dibujar);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.text_y2);
+            this.groupBox1.Controls.Add(this.text_x2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.text_x1);
+            this.groupBox1.Controls.Add(this.text_y1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 424);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de las coordenadas";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(121, 275);
+            this.maskedTextBox1.Mask = "0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 14;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Costo";
+            // 
+            // b_dibujar
+            // 
+            this.b_dibujar.Location = new System.Drawing.Point(91, 340);
+            this.b_dibujar.Name = "b_dibujar";
+            this.b_dibujar.Size = new System.Drawing.Size(75, 35);
+            this.b_dibujar.TabIndex = 12;
+            this.b_dibujar.Text = "Dibujar Arista";
+            this.b_dibujar.UseVisualStyleBackColor = true;
+            this.b_dibujar.Click += new System.EventHandler(this.b_dibujar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Destino";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Coordenada 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Origen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Coordenada 1:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(124, 219);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // text_y2
+            // 
+            this.text_y2.Location = new System.Drawing.Point(23, 45);
+            this.text_y2.Name = "text_y2";
+            this.text_y2.Size = new System.Drawing.Size(10, 20);
+            this.text_y2.TabIndex = 4;
+            this.text_y2.Visible = false;
+            // 
+            // text_x2
+            // 
+            this.text_x2.Location = new System.Drawing.Point(6, 45);
+            this.text_x2.Name = "text_x2";
+            this.text_x2.Size = new System.Drawing.Size(12, 20);
+            this.text_x2.TabIndex = 3;
+            this.text_x2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // text_x1
+            // 
+            this.text_x1.AcceptsTab = true;
+            this.text_x1.BackColor = System.Drawing.Color.White;
+            this.text_x1.Location = new System.Drawing.Point(6, 19);
+            this.text_x1.Name = "text_x1";
+            this.text_x1.Size = new System.Drawing.Size(11, 20);
+            this.text_x1.TabIndex = 1;
+            this.text_x1.Visible = false;
+            // 
+            // text_y1
+            // 
+            this.text_y1.Location = new System.Drawing.Point(23, 19);
+            this.text_y1.Name = "text_y1";
+            this.text_y1.Size = new System.Drawing.Size(10, 20);
+            this.text_y1.TabIndex = 0;
+            this.text_y1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Proyecto2.Properties.Resources.mapa_de_honduras_por_departamentos;
+            this.pictureBox1.InitialImage = global::Proyecto2.Properties.Resources.mapa_de_honduras_por_departamentos1;
+            this.pictureBox1.Location = new System.Drawing.Point(272, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(542, 424);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label8
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.ForeColor = System.Drawing.Color.Navy;
+            this.lblOrigen.Location = new System.Drawing.Point(127, 88);
+            this.lblOrigen.Name = "label8";
+            this.lblOrigen.Size = new System.Drawing.Size(0, 13);
+            this.lblOrigen.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Navy;
+            this.label9.Location = new System.Drawing.Point(127, 196);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(814, 424);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox text_x2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox text_x1;
+        private System.Windows.Forms.TextBox text_y1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox text_y2;
+        private System.Windows.Forms.Button b_dibujar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblOrigen;
+    }
+}
+
---
+++ Proyecto2/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x1;
+        int x2;
+        int y1;
+        int y2;
+       
+
+        void dibujarArista()
+        {
+            Graphics grafico = pictureBox1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Purple, 2);
+            SolidBrush sb = new SolidBrush(Color.DarkRed);
+
+               x1 = Convert.ToInt16(text_x1.Text);
+               y1 = Convert.ToInt16(text_y1.Text);
+               x2 = Convert.ToInt16(text_x2.Text);
+               y2 = Convert.ToInt16(text_y2.Text);
+
+            //      Vertice VOrigen = this.GetVertice(x1,y1);
+            //
+
+            Vertice VDestino = this.GetVertice(x2, y2);
+            
+      //      misariastas.add(VOrigen,VDestino, costo):
+
+            
+
+            grafico.DrawLine(lapiz, x1, y1, x2, y2);
+            limpiar();
+
+        }
+        
+        void limpiar()
+        {
+            text_x1.Clear();
+            text_y1.Clear();
+            text_x2.Clear();
+            text_y2.Clear();
+            lblOrigen.Text = "";
+            label9.Text = "";
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+        
+            x1 = e.X;
+            y1 = e.Y;
+            x2 = e.X;
+            y2 = e.Y;
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics grafico = pictureBox1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Purple,2);
+            SolidBrush sb = new SolidBrush(Color.DarkRed);
+
+            /*   if(text_x1.Text=="" && text_y1.Text == "")
+               {
+
+                   text_x1.Text = Convert.ToString(x1);
+                   text_y1.Text = Convert.ToString(y1);
+                   grafico.DrawRectangle(lapiz, x1, y1, 4, 4);
+                   grafico.FillRectangle(sb, x1, y1, 4, 4);
+               }
+                   else if(text_x1.Text != "" && text_y1.Text != "")
+               {
+                   text_x2.Text = Convert.ToString(x2);
+                   text_y2.Text = Convert.ToString(y2);
+                   grafico.DrawRectangle(lapiz, x2, y2, 4, 4);
+                  grafico.FillRectangle(sb, x2, y2, 4,4 );
+
+               }*/
+        
+         
+           Vertice verticeSeleccionado = this.GetVertice(x1, y1);
+
+            if (lblOrigen.Text == "")
+            {
+                if (verticeSeleccionado == null)
+                {
+                    grafico.DrawRectangle(lapiz, x1, y1, 4, 4);
+                    grafico.FillRectangle(sb, x1, y1, 4, 4);
+                    this.misVertices.Add(new Vertice(x1, y1));//agregar el nombre del vertice
+
+                }
+                else {
+                    x1 = verticeSeleccionado.x;
+                    y1 = verticeSeleccionado.y;
+                }
+                text_x1.Text = Convert.ToString(x1);
+                text_y1.Text = Convert.ToString(y1);
+                lblOrigen.Text = "(" + text_x1.Text + "," + text_y1.Text + ")";
+            }
+            else if (lblOrigen.Text != "")
+            {
+                if (verticeSeleccionado == null)
+                {
+                    grafico.DrawRectangle(lapiz, x2, y2, 4, 4);
+                    grafico.FillRectangle(sb, x2, y2, 4, 4);
+                    this.misVertices.Add(new Vertice(x2, y2));//agregar el nombre del vertice
+
+                }
+                else
+                {
+                    x2 = verticeSeleccionado.x;
+                    y2 = verticeSeleccionado.y;
+                }
+                text_x2.Text = Convert.ToString(x2);
+                text_y2.Text = Convert.ToString(y2);
+                label9.Text = "(" + text_x2.Text + "," + text_y2.Text + ")";
+            }
+
+
+        }
+
+
+        Vertice GetVertice(int x, int y)
+        {
+
+            foreach (Vertice v in misVertices)
+                if (x < v.x + 15 && x > v.x - 15 && y < v.y + 15 && y > v.y - 15)
+                    return v;
+               return null;
+        }
+
+        List<Vertice> misVertices = new List<Vertice>();
+        class Vertice {
+            public int x, y;
+            public String nombre;
+            public Vertice(int x, int y) {
+                this.x = x;
+                    this.y = y;
+            }
+        }
+
+        class Arista {
+            Vertice origen, destino;
+        }
+        private void b_dibujar_Click(object sender, EventArgs e)
+        {
+
+            dibujarArista();
+        }
+    }
+    }
+
